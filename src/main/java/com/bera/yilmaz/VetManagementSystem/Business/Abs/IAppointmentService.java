5e0c19a449406747d69f18a59eb57a4a578b40d5
BeraYilmaZzz <berayilmazbm@gmail.com>
2024-03-19 15:07:07 +0300

    lk commit

---
+++ src/main/java/com/bera/yilmaz/VetManagementSystem/Business/Abs/IAppointmentService.java
@@ -0,0 +1,32 @@
+package com.bera.yilmaz.VetManagementSystem.Business.Abs;
+
+import com.bera.yilmaz.VetManagementSystem.Dto.Response.AppointmentResponse;
+import com.bera.yilmaz.VetManagementSystem.Entitiy.Animal;
+import com.bera.yilmaz.VetManagementSystem.Entitiy.Appointment;
+import com.bera.yilmaz.VetManagementSystem.Entitiy.Doctor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface IAppointmentService {
+    Appointment save(Appointment appointment);
+    Appointment update(long id, Appointment appointment);
+    Appointment delete(long id);
+    List<Appointment> getAll();
+    Doctor getDoctorById(long id);
+
+    List<Appointment> getAppointmentsByDateAndDoctor(LocalDateTime appointmentDate, LocalDateTime endOfDay, long doctorId);
+
+    List<AppointmentResponse> getAppointmentsByDateAndAnimalId(LocalDateTime dateTime, LocalDateTime endOfDay, long animalId);
+
+    Doctor getDoctorByDoctorId(Long id);
+
+    Animal getAnimalByAnimalId(Long id);
+
+    List<Appointment> getDateByDoctorId(long doctorId);
+
+    List<Appointment> getAppointmentByReportId(long appointmentId);
+
+    List<Appointment> getAppointmentsByDoctorIdAndDate(Long doctorId, LocalDateTime localDate);
+}
